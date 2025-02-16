d758813d5c50ff3ef30b7a09b53f778cf8ede502
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-02-16 13:29:54 +0100

    Update wiki.js

--- js/wiki.js
+++ js/wiki.js
@@ -1,18 +1,23 @@
+// Wikidata API Endpoint
 const apiEndpoint = "https://www.wikidata.org/w/api.php"
 
+// Ottieni numero casuale per quesry SPARQL
 const getRandomInt = async (min, max) => {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+// Ottieni Item casuale che rispetti determinati criteri
 const randomItem = async () => {
 
-    let i = await getRandomInt(1, 1000)
+    let i = await getRandomInt(1, 1000) // Numero randomico per offset (ORDER BY RAND() rallenta di molto la query)
 
+    // Query SPARQL, fonte: https://www.wikidata.org/wiki/Property_talk:P396 --> Queries for homonyms
     let query = `
     SELECT DISTINCT ?item ?itemLabel
-    WHERE {
+    WHERE
+    {
         # Filtra gli item con cittadinanza italiana (Q38) o italiana (Q172579)
         VALUES ?v { wd:Q172579 wd:Q38 }
         ?item wdt:P27 ?v ;  # P27 = paese di cittadinanza
@@ -23,20 +28,14 @@ const randomItem = async () => {
 
         # Esclude gli item che hanno una proprietà P396
         FILTER NOT EXISTS { ?item wdt:P396 [] . }
-
-        # Esclude gli item che compaiono nella proprietà "consiste di" (P527) dell'item Q132256514 (WikiPlayground SBN NoMatch DB)
-        FILTER NOT EXISTS {
-            wd:Q132256514 wdt:P527 ?item .
-        }
-
     }
     OFFSET ${i}
     LIMIT 1    
     `;
 
+    // Chiamata API
     let wikiUrl = 'https://query.wikidata.org/sparql?query=' + encodeURIComponent( query );
     let headers = { 'Accept': 'application/sparql-results+json' };
-
     let response = await fetch( wikiUrl, { headers } );
     let json = await response.json();
 
@@ -44,31 +43,23 @@ const randomItem = async () => {
 
 };
 
+// Ottieni token per autenticazione
 const getWikiToken = async () => {
 
     let url = `${apiEndpoint}?action=query&meta=tokens&format=json`
-    let response = await fetch(url, {
-        credentials: "include"
-    })
+    let response = await fetch(url, { credentials: "include" })
     let json = await response.json()
     let token = json.query.tokens.csrftoken
 
-    if (token == "+\\") {
-        return null
-    } else {
-        return token
-    }
+    return null ? token == "+\\" : token
 
 }
 
-const editWikiItem = async (wikiItemId, opacList, token) => {
+// Contenuto richiesta per aggiornamento proprietà P396
+const claimBodyValue = async (opacList, currentDate) => {
     
     let claims = []
-
-    let currentDate = new Date().toISOString().split('T')[0]
-    
     opacList.forEach(opacEntity => {
-        
         let claimBody = {
             "mainsnak": {
                 "snaktype": "value",
@@ -80,13 +71,14 @@ const editWikiItem = async (wikiItemId, opacList, token) => {
                 }
             },
             "type": "statement",
+            "rank": "normal",
             "qualifiers": {
-                "P1810": [
+                "P1810": [ // soggetto indicato come
                     {
                         "snaktype": "value",
                         "property": "P1810",
                         "datavalue": {
-                            "value": opacEntity.Label,
+                            "value": opacEntity.Label, // Etichetta SBN
                             "type": "string"
                         }
                     }
@@ -114,61 +106,25 @@ const editWikiItem = async (wikiItemId, opacList, token) => {
                         ]
                     }
                 }
-            ],
-            "rank": "normal"
+            ]
         }
 
         claims.push(claimBody)
 
     });
-
-    const params = new URLSearchParams({
-        action: "wbeditentity",
-        id: wikiItemId,
-        token: token,
-        format: "json",
-        data: JSON.stringify({"claims":claims}),
-        summary: "WikiPlayground" 
-    })
-
-    const option = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/x-www-form-urlencoded"
-        },
-        body: params,
-        credentials: "include" 
-    }
-
-    let response = await fetch(apiEndpoint, option)
-    let rjson = await response.json()
-    if (rjson.success === 1) {
-        return "Item modificato con successo!"
-    } else {
-        return "Errore: " + JSON.stringify(rjson)
-    }
-
+    return claims
 }
 
-const noMatchItem = async (wikiItemId, token) => {
-    
+// Contenuto richiesta per item senza match sbn 
+const claimBodyNoMatchValue = async (currentDate) => {
     let claims = []
-
-    let currentDate = new Date().toISOString().split('T')[0]
-    
     let claimBody = {
         "mainsnak": {
-            "snaktype": "value",
-            "property": "P527",
-            "datavalue": {
-                "value":{
-                    "entity-type": "item",
-                    "id": wikiItemId
-                }, 
-                "type": "wikibase-entityid"
-            }
+            "snaktype": "novalue", // creazione poprietà con valore 'nessun valore'
+            "property": "P396"
         },
         "type": "statement",
+        "rank": "normal",
         "references": [
             {
                 "snaks": {
@@ -191,20 +147,40 @@ const noMatchItem = async (wikiItemId, token) => {
                     ]
                 }
             }
-        ],
-        "rank": "normal"
+        ]
     }
 
     claims.push(claimBody)
 
+    return claims
+}
+
+// Esegui modifica a elemento tramite chiamata API
+const editWikiItem = async (wikiItemId, opacMatchList, token) => {
+    
+    // Data corrente
+    let currentDate = new Date().toISOString().split('T')[0]
+
+    let claims
+
+    //Creazione corpo richiesta
+    if (opacMatchList.length > 0) { // se c'è almeno un match valido viene inserito
+        claims = await claimBodyValue(opacMatchList, currentDate)
+    } else if (opacMatchList.length === 0) { // se non ci sono match viene aggiunto il valore 'novalue'
+        claims = await claimBodyNoMatchValue(currentDate)
+    }
+    
+    // Parametri URL
     const params = new URLSearchParams({
         action: "wbeditentity",
-        id: "Q132256514",
+        id: wikiItemId,
         token: token,
         format: "json",
-        data: JSON.stringify({"claims":claims}) 
+        data: JSON.stringify({"claims":claims}),
+        summary: "WikiPlayground" // per successivo riconoscimento
     })
 
+    // Header chiamata
     const option = {
         method: "POST",
         headers: {
@@ -214,17 +190,17 @@ const noMatchItem = async (wikiItemId, token) => {
         credentials: "include" 
     }
 
+    // Esecuzione chiamata POST
     let response = await fetch(apiEndpoint, option)
     let rjson = await response.json()
     if (rjson.success === 1) {
         return true
     } else {
-        console.log("Errore:", JSON.stringify(rjson))
+        console.log(rjson)
         return false
     }
 
 }
 
-
-
-export { randomItem, getWikiToken, editWikiItem, noMatchItem }
+// Esportazione funzioni
+export { randomItem, getWikiToken, editWikiItem }

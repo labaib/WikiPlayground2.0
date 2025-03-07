553e0b3ffe86b6f3adbd65ea5951920ea740a910
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-03-07 21:17:49 +0100

    Update opac.js

--- js/opac.js
+++ js/opac.js
@@ -5,32 +5,29 @@ const opacApi = async (itemLabel) => {
     url += `core=autori`
     url += `&item%3A6003%3ANome=${encodeURIComponent(itemLabel)}`
     url += `&filter_nocheck:6021:Tipo_nome=Persona:A`
-    
-    let opacResponse = await fetch(url, { 
-        credentials: "include", 
-        headers: {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:95.0) Gecko/20100101 Firefox/95.0",
-            "Accept": "application/json, text/javascript, */*; q=0.01",
-            "Accept-Language": "it,en-US;q=0.7,en;q=0.3",
-            "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "X-Requested-With": "XMLHttpRequest",
-            "Sec-Fetch-Dest": "empty",
-            "Sec-Fetch-Mode": "cors",
-            "Sec-Fetch-Site": "same-origin",
-            "Pragma": "no-cache",
-            "Cache-Control": "no-cache"
+
+    chrome.runtime.sendMessage(
+        {
+            action: "callApi",
+            url: url, // URL dell'API
+            method: "GET", // Metodo HTTP
+            headers: { "Content-Type": "application/json" }, // Intestazioni
+            body: JSON.stringify({ key: "value" }) // Corpo della richiesta (opzionale)
         },
-        referrer: `https://opac.sbn.it/risultati-autori?core=autori&item%3A6003%3ANome=${encodeURIComponent(itemLabel)}`,
-		mode: "cors"
-    });
-    let opacJson = await opacResponse.json();
-    let resultList = opacJson.data.results
-    
-    if (resultList.length > 0) {
-        return resultList
-    } else {
-        return []
-    }
+        (response) => {
+            if (response.success) {
+                let resultList = response.data.results
+                
+                if (resultList.length > 0) {
+                    return resultList
+                } else {
+                    return []
+                }
+            } else {
+                console.error("Errore:", response.error);
+            }
+        }
+    );
 
 } 
 
